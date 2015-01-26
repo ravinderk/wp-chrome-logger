63f9e9f7cabb27d754a85884b2e1803cc1f997ec
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2015-01-27 00:49:38 +0530

    now you can use this plugin in WordPress backend for logging data  in chrome console.

--- README.md
+++ README.md
@@ -3,16 +3,20 @@ wp-chrome-debugger
 
 Thank to [Craig Campbell] for creating awesome debugging tool [chromephp]
 
-How to work
-=============
-Download this plugin and activate it.
+## Installation
+1. Install the Chrome extension from: https://chrome.google.com/extensions/detail/noaneddfkdjfnfdakjjmocngnfkfehhd
+2. Click the extension icon in the browser to enable it for the current tab's domain
+3. Download this plugin and activate it.
 
+##How to work
 Use any function ( below in example ) to output anything in chrome console ( put these functions in ```plugin file``` or ```functions.php``` ).
 ```sh
 ChromePhp::log('Hello console!');
 ChromePhp::log($_SERVER);
 ChromePhp::warn('something went wrong!');
 ```
+
+**Note:**  Use these functions under a WordPress hook before header sent otherwise you will get an *header already sent* error in WordPress.
 
 For more [info visit this link][1]
 [1]:https://github.com/ccampbell/chromephp
--- chrome-logger.php
+++ chrome-logger.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: WP Chrome Logger
 Description: output anything in chrome console ( yet another debugging helper )
-Plugin URI: http://blogdesignstudio.com
-Author: Mayank Gupta / Ravinder Kumar
-Author URI: http://blogdesignstudio.com
-Version: 0.1
+Plugin URI:
+Author: Ravinder Kumar
+Author URI:
+Version: 0.2
 License: GPL2
 Text Domain:
 Domain Path:
@@ -29,41 +29,43 @@ Domain Path:
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-/**
- * [chrome_logger_init description]
- *
- * @since 0.1
- * @package chrome logger
- */
-function chrome_logger_init() {
-	ob_start();
-	require_once "chromephp/ChromePhp.php";
-}
-add_action( 'init', 'chrome_logger_init' );
 
-/**
- * [chrome_logger_footer_output description]
- *
- * @since 0.1
- * @package  chrome logger
- */
-function chrome_logger_footer_end() {
-	ob_end_flush();
-}
-add_action( 'wp_footer', 'chrome_logger_footer_end', 999999 );
+class MB_Ravs_Chrome_Logger{
+	function __construct(){
 
-/**
- * [chrome_logger_footer_test description]
- *
- * @since 0.1
- * @package chrome logger
- */
-function chrome_logger_footer_test() {
-	$user = wp_get_current_user();
-	ChromePhp::log( get_posts() );
-	ChromePhp::log( 'Hello console!' );
-	ChromePhp::log( $_SERVER );
-	ChromePhp::warn( 'something went wrong!' );
+		//include chromePHP on frontend
+		add_action( 'init', array( $this, 'add_chrome_php' ) );
+
+		//include chromePHP on backend
+		add_action( 'admin_init', array( $this, 'add_chrome_php' ) );
+
+		//test
+//		add_action( 'template_redirect', array( $this, 'frontend_test' ) );
+//		add_action( 'admin_init', array( $this, 'frontend_test' ) );
+	}
+
+	/**
+	 * include chromePHP file
+	 *
+	 * @since 0.1
+	 * @package chrome logger
+	 */
+	function add_chrome_php() {
+		require_once "chromephp/ChromePhp.php";
+	}
+
+	/**
+	 * [chrome_logger_footer_test description]
+	 *
+	 * @since 0.1
+	 * @package chrome logger
+	 */
+	function frontend_test() {
+//		ChromePhp::log( get_posts() );
+		ChromePhp::log( 'Hello console!' );
+		ChromePhp::log( $_SERVER );
+		ChromePhp::warn( 'something went wrong!' );
+	}
 }
-// add_action( 'wp_footer', 'chrome_logger_footer_test' );
-?>
+
+new MB_Ravs_Chrome_Logger();
